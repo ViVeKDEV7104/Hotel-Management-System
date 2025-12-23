f718684a2af035d15c5df2253805bc45af89946a
ViVeKDEV7104 <vivxk.singh7104@gmail.com>
2025-12-24 04:49:01 +0530

    Add files via upload

---
+++ Hotel Management System/src/hotel/management/system/conn.java
@@ -0,0 +1,24 @@
+package hotel.management.system;
+
+
+import java.sql.*;  
+
+
+
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///hotelmanagementsystem","root","Dumbfucked@7104");
+            s =c.createStatement();  
+        
+        }
+        catch(Exception e){ 
+            e.printStackTrace();
+        }  
+    }  
+}  
+
